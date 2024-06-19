5a78af60897daab6ae4430676d32f25e7ddc7310
Alex Mitra <alexmitra@chromium.org>
2024-06-19 10:44:04 +0100

    Update test file.

--- src/main/java/org/chromium/chromium_test/chromiumtest/DummyTest.java
+++ src/main/java/org/chromium/chromium_test/chromiumtest/DummyTest.java
@@ -4,8 +4,12 @@ import org.junit.Test;
 import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.RunWith;
 
+/**
+ * This is a test class that does nothing. Its purpose is to test that the plugin is working
+ * properly in the Intellij sandbox.
+ */
 @RunWith(JUnit4ClassRunner.class)
-public class DownloadInterstitialMediatorTest {
+public class DummyTest {
 
   @Test
   public void testFoo() {
