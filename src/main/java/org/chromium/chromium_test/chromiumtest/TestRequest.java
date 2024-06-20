96d786385c1bc0779d720a473ff5f739de1a342c
Alex Mitra <alexmitra@chromium.org>
2024-06-20 19:58:47 +0100

    Add a new TestRequest struct.

---
+++ src/main/java/org/chromium/chromium_test/chromiumtest/TestRequest.java
@@ -0,0 +1,30 @@
+package org.chromium.chromium_test.chromiumtest;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A simple, immutable struct which stores all the data needed about a given test or suite of tests.
+ */
+public class TestRequest {
+  private final @NotNull String mFileName;
+  private final @NotNull String mClassName;
+  private final @NotNull String mMethodName;
+
+  public TestRequest(@NotNull String fileName, @NotNull String className, @NotNull String methodName) {
+    mFileName = fileName;
+    mClassName = className;
+    mMethodName = methodName;
+  }
+
+  public @NotNull String fileName() {
+    return mFileName;
+  }
+
+  public @NotNull String className() {
+    return mClassName;
+  }
+
+  public @NotNull String methodName() {
+    return mMethodName;
+  }
+}
