ae28b4ef2a9b6dcaebf980e3c8778341e8903f37
Alex Mitra <alexmitra@chromium.org>
2024-06-20 19:44:30 +0100

    Add new action for bisecting tests.

---
+++ src/main/java/org/chromium/chromium_test/chromiumtest/BisectTestAction.java
@@ -0,0 +1,27 @@
+package org.chromium.chromium_test.chromiumtest;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbAware;
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+public class BisectTestAction extends AnAction implements DumbAware {
+  private final String mFileName;
+  private final String mClassName;
+  private final String mMethodName;
+
+  BisectTestAction(String fileName, String className, String methodName) {
+    super(
+        String.format("Find when '%s' started failing", StringUtils.isNotEmpty(methodName) ? methodName : className),
+        "Bisects the selected tests to find when they started failing.",
+        AllIcons.Debugger.ShowCurrentFrame);
+    mFileName = fileName;
+    mClassName = className;
+    mMethodName = methodName;
+  }
+
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent e) {}
+}
